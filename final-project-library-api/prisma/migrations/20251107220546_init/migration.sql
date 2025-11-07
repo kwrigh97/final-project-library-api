57da556674676bfc8568f6fce6e679479a731292
Kaewmanee Wright <kwrigh97@charlotte.edu>
2025-11-07 18:58:52 -0500

    Implement database

---
+++ final-project-library-api/prisma/migrations/20251107220546_init/migration.sql
@@ -0,0 +1,66 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('member', 'librarian');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Loan" (
+    "id" SERIAL NOT NULL,
+    "bookId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "loanedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dueAt" TIMESTAMP(3) NOT NULL,
+    "returnedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Loan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "isbn" TEXT NOT NULL,
+    "publishedYear" INTEGER NOT NULL,
+    "copiesTotal" INTEGER NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Author" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Author_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BookAuthor" (
+    "bookId" INTEGER NOT NULL,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "BookAuthor_pkey" PRIMARY KEY ("bookId","authorId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Loan" ADD CONSTRAINT "Loan_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Loan" ADD CONSTRAINT "Loan_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BookAuthor" ADD CONSTRAINT "BookAuthor_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BookAuthor" ADD CONSTRAINT "BookAuthor_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ final-project-library-api/prisma/seed.js
@@ -0,0 +1,79 @@
+import prisma from '../src/config/db.js';
+
+async function main() {
+    await prisma.$queryRaw`TRUNCATE "Loan", "BookAuthor", "Book", "Author", "User" RESTART IDENTITY;`;
+    const librarian = await prisma.user.create({
+        data: {
+            name: 'Alice Johnson',
+            email: 'alicej@library.com',
+            role: 'librarian',
+        },
+    });
+
+    const member = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'JohnDoe@member.com',
+            role: 'member',
+        },
+    });
+
+    const author1 = await prisma.author.create({
+        data: {name: 'Suzanne Collins'},
+    });
+
+    const author2 = await prisma.author.create({
+        data: {name: 'S. E. Hinton'},
+    });
+
+    const book1 = await prisma.book.create({
+        data: {
+            title: 'Hunger Games',
+            isbn: '1234',
+            publishedYear: 2008,
+            copiesTotal: 10,
+        },
+    });
+
+    const book2 = await prisma.book.create({
+        data: {
+            title: 'The Outsiders',
+            isbn: '5678',
+            publishedYear: 1967,
+            copiesTotal: 5,
+        },
+    });
+
+    await prisma.loan.create({
+        data: {
+            bookId: book1.id,
+            userId: member.id,
+            loanedAt: new Date(),
+            dueAt: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000), //Automatically makes the due date 2 weeks from loan date
+        },
+    });
+
+    await prisma.bookAuthor.createMany({
+        data: [
+            {
+                bookId: book1.id,
+                authorId: author1.id,
+            },
+            {
+                bookId: book2.id,
+                authorId: author2.id,
+            },
+        ],
+    });
+
+    console.log('Database seeded successfully!');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
