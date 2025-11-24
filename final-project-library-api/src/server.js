7a5ff4ad8e97857832a80df34b312f6cbad07afa
ryan <west.m.ryan@gmail.com>
2025-11-24 16:22:08 -0500

    Implemented Swagger for Book and Author Endpoints

--- final-project-library-api/src/server.js
+++ final-project-library-api/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +11,9 @@ app.use(cors());
 app.use(morgan('tiny'));
 
 app.use(express.json());
+
+const swaggerDocument = YAML.load("./src/docs/openapi.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
